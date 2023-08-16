db699cb35aaaaf922976d7fd9451a30e583fbe8b
AminBhst <amin.bhst@gmail.com>
2023-08-16 09:22:05 -0400

    Fix transparency

--- lua/ryewell/options.lua
+++ lua/ryewell/options.lua
@@ -1,3 +1,21 @@
+-- Fix transparency in colors schemes
+vim.api.nvim_create_autocmd('ColorScheme', {
+  callback = function()
+    local highlights = {
+      'Normal',
+      'LineNr',
+      'Folded',
+      'NonText',
+      'SpecialKey',
+      'VertSplit',
+      'SignColumn',
+      'EndOfBuffer',
+      'TablineFill', -- this is specific to how I like my tabline to look like
+    }
+    for _, name in pairs(highlights) do vim.cmd.highlight(name .. ' guibg=none ctermbg=none') end 
+end, 
+})
+
 local options = {
   backup = false,                          -- creates a backup file
   clipboard = "unnamedplus",               -- allows neovim to access the system clipboard

b4bab1effc5b186ca9f76966b28e06e85d957342
AminBhst <amin.bhst@gmail.com>
2024-02-24 18:31:20 +0330

    Harpoon changes

--- lua/ryewell/harpoon.lua
+++ lua/ryewell/harpoon.lua
@@ -1,8 +1,10 @@
   require("harpoon").setup({
     menu = {
-        width = vim.api.nvim_win_get_width(0) - 50,
+        width = vim.api.nvim_win_get_width(0) - 40,
     }
 })
 
 vim.keymap.set("n", "<space>ha", "<Cmd>:lua require('harpoon.mark').add_file()<CR>", {silent = true})
 vim.keymap.set("n", "<space>hm", "<Cmd>:lua require('harpoon.ui').toggle_quick_menu()<CR>", {silent = true})
+vim.keymap.set("n", "<space>hn", "<Cmd>:lua require('harpoon.ui').nav_next()<CR>", {silent = true})
+vim.keymap.set("n", "<space>hp", "<Cmd>:lua require('harpoon.ui').nav_prev()<CR>", {silent = true})
